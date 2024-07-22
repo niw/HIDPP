34999a0bb49f6596055191686c630099f9ce5924
Yoshimasa Niwa <niw@niw.at>
2024-07-22 10:52:40 -0700

    Update Swift tool version and argument parser.

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -25,7 +25,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/apple/swift-argument-parser",
-            from: "1.3.0"
+            from: "1.5.0"
         )
     ],
     targets: [
